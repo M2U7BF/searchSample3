002c2bcc7866acb564245f7f0d2ca4b1a2cac5b3
M2U7BF <yhvh4321@gmail.com>
2022-08-28 12:57:50 +0900

    [更新]

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,9 +1,13 @@
 const userCardTemplate = document.querySelector("[data-user-template]") //template
+const userDataListTemplate = document.querySelector("[data-list-elem-template]") //template
+
 const userCardContainer = document.querySelector("[data-user-cards-container]") //大枠。この中で要素らが動く
+const userDataList = document.querySelector("[search-data-list]")
 
 const searchInput = document.querySelector("[data-search]") //検索欄
 
 let users = []
+let dataElements = []
 
 searchInput.addEventListener("input", e => {
     console.log("クリック")
@@ -16,12 +20,32 @@ searchInput.addEventListener("input", e => {
             user.number.includes(value)
         user.element.classList.toggle("hide", !isVisible)
     })
+
+    dataElements.forEach(dataElement => {
+        const isVisible =
+            dataElement.name.toLowerCase().includes(value) ||
+            dataElement.number.includes(value)
+        dataElement.element.classList.toggle("disable", !isVisible)
+    })
 })
 
 //fetch()で非同期通信により、外部リソースの取得
 fetch("http://localhost:8080/json")
     .then(res => res.json())
     .then(data => {
+
+        dataElements = data.map(dataElement => {
+            const dataList = userDataListTemplate.content.cloneNode(true).children[0]
+
+            //文字列をセット
+            dataList.textContent = dataElement.number
+            dataList.value = dataElement.name
+
+            //html追加
+            userDataList.append(dataList)
+
+            return { id: dataElement.id, name: dataElement.name, number: dataElement.number.toString(), element: dataList }
+        })
 
         //配列にデータを格納
         users = data.map(user => {
